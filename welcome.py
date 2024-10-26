718cec9901ec5239239399cb65b33553cbe38fa1
Devendranathashok <ashok.kammala@gmail.com>
2024-10-26 12:41:49 +0530

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -3,6 +3,6 @@ def hello_name(name):
   greet = "Hello "+ name
   return greet
   
-hello_name('Bob')
-hello_name('Alice')
-hello_name('X')
+print(hello_name('Bob'))
+print(hello_name('Alice'))
+print(hello_name('X'))
