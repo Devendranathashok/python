31ff51e7c4ba53d0c708cfe477e6e80efe335f10
Devendranathashok <ashok.kammala@gmail.com>
2024-11-02 12:00:45 +0530

    Create swapping.py

---
+++ swapping.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+def swapnum():
+    a = int(input("Please provide a value for a:"))
+    b = int(input("Please provide a value for b:"))
+    c = a
+    a = b
+    b = c
+    print('a=', a ,'b=', b)
+
+swapnum()  
